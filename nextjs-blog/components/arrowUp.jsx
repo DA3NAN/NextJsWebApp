a0a9711975b341207bc0b4eaa498825d5aece16e
floki303 <adnane.aitmalek20@gmail.com>
2023-04-12 18:16:19 +0000

    init

--- nextjs-blog/components/arrowUp.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { ChevronUpIcon } from "@heroicons/react/24/solid";
-
-export default function arrowUp() {
-  return <ChevronUpIcon height={20} width={20} color="black" />;
-}
